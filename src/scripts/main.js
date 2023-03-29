250f3f32d4bf8eb6e7c5d5d097ba8ecc4afcd507
Konstantin0 <maschkovc@yandex.ru>
2023-03-29 12:26:51 +0300

    media

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 
+window.addEventListener("DOMContentLoaded", () => {
+	const menu = document.querySelector(".header__wrapper"),
+		hamburger = document.querySelector(".hamburger");
+
+	hamburger.addEventListener("click", () => {
+		hamburger.classList.toggle("hamburger_active");
+		menu.classList.toggle("header__wrapper_active");
+	});
+});
